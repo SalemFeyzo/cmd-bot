396cfa492ba67e4166d5107cdd1c62b7bb525a5a
salemFeyzo <f1989salim@gmail.com>
2022-06-28 19:47:34 +0300

    h

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const ccxt = require("ccxt");
 const GateApi = require("gate-api");
 
 const ccxtGateIoClient = new ccxt.gateio({
+  enableRateLimit: true,
   apiKey: process.env.GATE_IO_API_KEY,
   secret: process.env.GATE_IO_SECRET,
 });
@@ -15,9 +16,9 @@ client.setApiKeySecret(
 const api = new GateApi.SpotApi(client);
 
 // string | Currency pair
-const currencyPair = "ARSW_USDT";
+const currencyPair = "TEDDY_USDT";
 const baseCurrency = "USDT";
-const currencyToBuy = "ARSW"; // "REVOLAND" , "WLKN" , "BTC"
+const currencyToBuy = "TEDDY"; // "REVOLAND" , "WLKN" , "BTC"
 
 const ticker = async () => {
   try {
@@ -35,46 +36,56 @@ const ticker = async () => {
     const buyVolume = availableUSDT / price; // return how much curr to buy
     const sellVolume = boughtCurr * price; // return usdt
     const tradeStatus = currPairs.body.tradeStatus;
-
-    await api.cancelOrders(currencyPair, {
-      side: "buy",
-      account: "spot",
-    });
-    await api.cancelOrders(currencyPair, {
-      side: "sell",
-      account: "spot",
-    });
+    const orders = await api.listAllOpenOrders();
+    if (orders) {
+      await api.cancelOrders(currencyPair, {
+        side: "buy",
+        account: "spot",
+      });
+      await api.cancelOrders(currencyPair, {
+        side: "sell",
+        account: "spot",
+      });
+    }
 
     const market = `${currencyToBuy}/${baseCurrency}`;
     console.log("buyVolume: ", buyVolume);
     console.log("sellVolume: ", sellVolume);
     console.log("USDT: ", availableUSDT);
-    console.log("ARSW: ", boughtCurr);
+    console.log("TEDDY: ", boughtCurr);
     console.log("Price: ", price);
 
-    if (price <= 0.012 && availableUSDT > 2) {
-      const order = await ccxtGateIoClient.createLimitBuyOrder(
-        market,
-        buyVolume,
-        price + 0.0001
-      );
+    if (price <= 0.000221 && availableUSDT > 2) {
+      try {
+        const order = await ccxtGateIoClient.createLimitBuyOrder(
+          market,
+          buyVolume,
+          price
+        );
 
-      console.log("Buy status: ", order.status);
-    } else if (price > 0.0129 && sellVolume > 2) {
-      const order = await ccxtGateIoClient.createLimitSellOrder(
-        market,
-        boughtCurr,
-        price - 0.0001
-      );
+        console.log("Buy status: ", order.status);
+      } catch (error) {
+        console.log(error.message);
+      }
+    } else if (price > 0.000221 && sellVolume > 2) {
+      try {
+        const order = await ccxtGateIoClient.createLimitSellOrder(
+          market,
+          boughtCurr,
+          price
+        );
 
-      console.log("Sell status: ", order.status);
+        console.log("Sell status: ", order.status);
+      } catch (error) {
+        console.log(error.message);
+      }
     } else {
       console.log("Order status: Failed");
     }
   } catch (error) {
-    console.error(error);
+    console.error(error.message);
   }
 };
 
 ticker();
-setInterval(ticker, 1000);
+setInterval(ticker, 2000);
